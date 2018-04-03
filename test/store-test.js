be3efedabc7a1adfe7aeb89a0d3b48d4005c8a17
victorbofill <victorm.bofill@gmail.com>
2018-04-03 16:56:31 -0700

    created initial store files

---
+++ test/store-test.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+const store = requite('../lib/store');
+
+describe('Store method tests', () => {
+    it('.save', () => {
+
+        assert.equal(true, true);
+    });
+
+    // it('.get', () => {
+
+    // });
+
+    // it('.getAll', () => {
+
+    // });
+
+    // it('.remove', () => {
+
+    // });
+});
