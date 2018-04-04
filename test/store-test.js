ca874acc7a733a3571755462c400d4fd9e7aaa75
victorbofill <victorm.bofill@gmail.com>
2018-04-03 17:10:07 -0700

    Wrote todos

--- test/store-test.js
+++ test/store-test.js
@@ -4,19 +4,30 @@ const uniqid = require('uniqid');
 
 describe('Store method tests', () => {
     it('.save', () => {
+        // run the save method
+        // check the store that whatever has been saved is in the store
 
         assert.equal(true, true);
     });
 
     // it('.get', () => {
 
+    // run the get method
+    // verify that the attempted get by id has the name it should
+
     // });
 
     // it('.getAll', () => {
 
+    // run the getAll method
+    // check to make sure that what is returned is an array with everything in it
+
     // });
 
     // it('.remove', () => {
+
+    // run the remove method
+    // check to ensure that the removed id is no longer in the store
 
     // });
 });
